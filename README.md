6dc999e6e002afbdf9c0e9294914ea2d61faafa3
mnansary <silentassasin0111@gmail.com>
2020-01-18 02:38:56 +0600

    scr calculation

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # pyPSSGA
 Power System Simulation with PSSE
+
+        Author:MD. Nazmuddoha Ansary, Sajjad Uddin Mahmud
+
+# Environment
+
+        PSSEXplore:340302
+        python: 2.7.13
+        pip: 9.0.1
+
+* ```pip install -r requirements.txt```
+
+        astroid==1.6.6
+        backports.functools-lru-cache==1.6.1
+        colorama==0.4.3
+        configparser==4.0.2
+        enum34==1.1.6
+        futures==3.3.0
+        isort==4.3.21
+        lazy-object-proxy==1.4.3
+        mccabe==0.6.1
+        numpy==1.16.6
+        pandas==0.24.2
+        pylint==1.9.5
+        python-dateutil==2.8.1
+        pytz==2019.3
+        singledispatch==3.4.0.3
+        six==1.14.0
+        termcolor==1.1.0
+        wrapt==1.11.2
+
+# Setup
+* Add **python27** binary path to **environment variables**
+* Create a **_some_name_.pth** (*eg:_psse_.pth*) file in **Site-packages** dir of python installation dir *example:C:\Python27\Lib\site-packages*
+* **_psse_.pth** should contain: **C:\Program Files (x86)\PTI\PSSEXplore34\PSSPY27** 
+
+# SCR Calculation
+* Edit values in **config.json**
+    * CASE_PATH   : path to case file (.sav)
+    * BUS_IDS     : list of relevant high voltage buses 
+    * MACHINE_IDS : list of relevant generator buses 
+
+* example *config.json*:
+
+        {
+            "CASE_PATH"     :"F:\\PSSE\\code\\case.sav",
+            "BUS_IDS"       :"14,15,16,20",
+            "GEN_IDS"       :"30,36,32,34"  
+        }
+
+* execute: ``` getSCR.py```
+> change dir if necessary
+
+* Example Execution results
+
+![](/src_img/exec.PNG?raw=true)
---
+++ getSCR.py
@@ -0,0 +1,165 @@
+ # -*- coding: utf-8 -*-
+'''
+@author: MD. Nazmuddoha Ansary, Sajjad Uddin Mahmud
+'''
+from __future__ import print_function
+import psspy
+import pssarrays
+import redirect
+import sys
+import os
+import numpy as np
+import pandas as pd
+import math
+import json
+from time import time
+import contextlib
+from termcolor import colored
+os.system('color')
+#-----------------------------------------------------------------------------------------------------------
+redirect.psse2py()
+psspy.throwPsseExceptions=True
+OLD_STDOUT = sys.stdout  
+@contextlib.contextmanager  
+def silence(new_target=None):
+    if new_target is None:
+        new_target = open(os.devnull, 'w')
+    old_target, sys.stdout = sys.stdout, new_target # replace sys.stdout
+    try:
+        yield new_target # run some code with the replaced stdout
+    finally:
+        sys.stdout = old_target # restore to the previous value
+#-----------------------------------------------------------------------------------------------------------
+def LOG_INFO(msg,pcolor='green'):
+    print(colored('#LOG:    ',color='blue'),colored(msg,color=pcolor))
+
+def readJson(file_name):
+    return json.load(open(file_name))
+
+def banner():
+    _stars='***********************************************'
+    _title='*              SCR Calculation                *'
+    _auth ='* MD. Nazmuddoha Ansary , Sajjad Uddin Mahmud *'  
+    print(colored(_stars,color='yellow'))
+    print(colored(_title,color='yellow'))
+    #print(colored(_auth,color='yellow'))
+    print(colored(_stars,color='yellow'))
+
+def __summary(hv_bus_ids,gen_bus_ids,pmax,i_sym,v_kv,v_pu,scr,save_csv=True):
+    # intialise data of lists. 
+    data = {'HV BUS':hv_bus_ids, 
+            'GEN BUS':gen_bus_ids,
+            'Pmax (MW)':pmax,
+            'I"(A)':i_sym,
+            'Voltage (kv)':v_kv,
+            'p.u(V)':v_pu}
+    # Create DataFrame 
+    df = pd.DataFrame(data)
+    df['SCR']=scr
+    LOG_INFO('Summary:') 
+    print(colored(df.head(len(hv_bus_ids)),color='yellow'))
+    if save_csv:
+        _csv_path=os.path.join(os.getcwd(),'summary.csv')
+        df.to_csv(_csv_path)
+        LOG_INFO('Saved Summary at: {}'.format(_csv_path))
+#-----------------------------------------------------------------------------------------------------------
+def getParams(case_study,hv_bus_ids,gen_bus_ids):
+    '''
+    ARGS: 
+        case_study  : path to case file (.sav) <STRING>
+        hv_bus_ids  : list of relevant high voltage buses <LIST OF INT>
+        gen_bus_ids : list of relevant generator buses <LIST OF INT>
+    RETURNS:
+        pmax        : list of max power for generator buses
+        i_sym       : list of 3 phase symmetric fault current magnitude for high voltage buses
+        v_kv        : list of base voltage in KV unit for high voltage busess
+        v_pu        : list of per unit voltage for high voltage buses
+    '''
+    # params
+    pmax=[]
+    i_sym=[]
+    v_kv=[]
+    v_pu=[]
+    # non-verbose execution
+    LOG_INFO('Initializing psspy')
+    with silence():
+        # initialize
+        psspy.psseinit(8000)
+    LOG_INFO('Loading Case ')
+    with silence():
+        # load case
+        psspy.case(case_study)
+    LOG_INFO('Solving for full newton-rafsan ')
+    with silence():
+        # full newton-rafsan
+        psspy.fnsl(options4=1,
+                   options5=1,
+                   options6=1)
+    LOG_INFO('Getting IECS values')
+    with silence():
+        # get symmetric 3-phase fault currents
+        all_currents=pssarrays.iecs_currents(all=1,
+                                            flt3ph=1,
+                                            optnftrc=2,
+                                            vfactorc=1.0)
+    # all bus pu voltage
+    _, (__v_pu,) = psspy.abusreal(sid=-1, string=["PU"])
+    # all bus kv voltage
+    _, (__v_kv,) = psspy.abusreal(sid=-1, string=["KV"])
+    # get pmax
+    for _id in gen_bus_ids:
+        _, _pmax = psspy.macdat(ibus=_id, id='1', string='PMAX') # find power of machine
+        pmax.append(_pmax)
+    # get v_pu,v_kv,i_sym
+    for _id in hv_bus_ids:
+        v_pu.append(__v_pu[_id-1])
+        v_kv.append(__v_kv[_id-1])
+        i_sym.append(all_currents.flt3ph[_id-1].ibsym.real)
+    
+    return pmax,i_sym,v_kv,v_pu
+
+def calcSCR(pmax,i_sym,v_kv,v_pu):
+    '''
+    ARGS:
+        pmax        : list of max power for generator buses
+        i_sym       : list of 3 phase symmetric fault current magnitude for high voltage buses
+        v_kv        : list of base voltage in KV unit for high voltage busess
+        v_pu        : list of per unit voltage for high voltage buses
+    RETURNS:
+        scr         : list of short circuit ratios
+    Calculation:
+        scr=sqrt(3)*i_sym*v_pu*v_kv*_UNIT_FACTOR/pmax
+        _UNIT_FACTOR=1e-3
+    '''
+    total_bus=len(pmax)
+    scr=[]
+    _UNIT_FACTOR=1e-3
+    LOG_INFO('Calculating SCR')
+    for idx in range(total_bus):
+        scr.append(math.sqrt(3)*i_sym[idx]*v_pu[idx]*v_kv[idx]*_UNIT_FACTOR/pmax[idx])
+    return scr
+
+
+#-----------------------------------------------------------------------------------------------------------
+if __name__=='__main__':
+    start_time=time()
+    banner()
+    LOG_INFO('Loading System Config ')
+    config_data=readJson('config.json')
+
+    CASE_STUDY=config_data['CASE_PATH']
+    BUS_IDS=str(config_data['BUS_IDS']).split(',')
+    BUS_IDS=[int(_id) for _id in BUS_IDS ]
+
+    MACHINE_IDS=str(config_data['GEN_IDS']).split(',')
+    MACHINE_IDS=[int(_id) for _id in MACHINE_IDS]
+
+    pmax,i_sym,v_kv,v_pu= getParams(case_study=CASE_STUDY,
+                                    hv_bus_ids=BUS_IDS,
+                                    gen_bus_ids=MACHINE_IDS)
+    
+    scr=calcSCR(pmax,i_sym,v_kv,v_pu)
+
+    __summary(BUS_IDS,MACHINE_IDS,pmax,i_sym,v_kv,v_pu,scr,save_csv=True)
+    
+    LOG_INFO('Time Taken:{}'.format(time()-start_time),pcolor='cyan')
