55f0bfa07d736ed5a24eb5218c2f0ca625552d51
mnansary <silentassasin0111@gmail.com>
2020-01-21 16:27:10 +0600

    Added GA

--- optimize.py
+++ optimize.py
@@ -41,12 +41,18 @@ def readJson(file_name):
     return json.load(open(file_name))
 
 def banner():
-    _stars='***********************************************'
-    _title='*              SCR Calculation                *'
-    _auth ='* MD. Nazmuddoha Ansary , Sajjad Uddin Mahmud *'  
+    _stars  ='***********************************************'
+    _title  ='*   Synchronous Condenser Size Optimization   *'
+    _method ='*         Method: Genetic Algorithm           *'
+    _auth_0 ='*                 Authors                     *'  
+    _auth_1 ='*          MD. Nazmuddoha Ansary              *'  
+    _auth_2 ='*           Sajjad Uddin Mahmud               *'  
     print(colored(_stars,color='yellow'))
     print(colored(_title,color='yellow'))
-    #print(colored(_auth,color='yellow'))
+    print(colored(_method,color='yellow'))
+    print(colored(_auth_0,color='yellow'))
+    print(colored(_auth_1,color='yellow'))
+    print(colored(_auth_2,color='yellow'))
     print(colored(_stars,color='yellow'))
 
 #-----------------------------------------------------------------------------------------------------------
@@ -63,6 +69,12 @@ SC_SIZE_MIN =   config_data['SC_SIZE_MIN']
 SC_SIZE_MAX =   config_data['SC_SIZE_MAX']
 STEP_SIZE   =   config_data['STEP_SIZE']
 THRESH      =   config_data['THRESH']
+POP_SIZE    =   config_data['POP_SIZE']
+MAX_ITER    =   config_data['MAX_ITER']
+
+INITIAL_SIZES=str(config_data['INITIAL_SIZES']).split(',')
+INITIAL_SIZES=[int(_size) for _size in INITIAL_SIZES ]
+INITIAL_SIZES=np.asarray(INITIAL_SIZES)
 #-----------------------------------------------------------------------------------------------------------
 def initCase():
     LOG_INFO('Initializing psspy')
@@ -91,71 +103,43 @@ def __summary(hv_bus_ids,gen_bus_ids,pmax,i_sym,v_kv,v_pu,scr,save_csv=True):
         _csv_path=os.path.join(os.getcwd(),'summary.csv')
         df.to_csv(_csv_path)
         LOG_INFO('Saved Summary at: {}'.format(_csv_path))
-
-def modifyCaseSingle(_id):
-    __sizes=range(SC_SIZE_MIN,SC_SIZE_MAX+STEP_SIZE,STEP_SIZE)
-    scrs=[]
-    LOG_INFO('Calculating SCR')
-    for __size in tqdm(__sizes):
+#-----------------------------------------------------------------------------------------------------------
+def modifyCaseBySize(sizes):
+    for idx in range(len(SC_IDS)):
         with silence():
-            psspy.machine_data_2(i=SC_IDS[_id],
-                                id='1',
-                                intgar=[1,0,0,0,0,0],
-                                realar=[0,0,500,-200,0,0,__size,0,0.09,0,0,1,1,1,1,1,1])
-        scr=calcSCR(hv_bus_ids=BUS_IDS,gen_bus_ids=MACHINE_IDS)
-        scrs.append(scr[_id])
-    
-    data = {'size':__sizes, 
-            'scr':scrs}
-    # Create DataFrame 
-    df = pd.DataFrame(data)
-    df.set_index('size',inplace=True)
-    print(df.head())
-    df.plot()
-    plt.show()
+            psspy.machine_data_2(i=SC_IDS[idx],
+                                 id='1',
+                                 intgar=[1,0,0,0,0,0],
+                                 realar=[0,0,500,-200,0,0,sizes[idx],0,0.09,0,0,1,1,1,1,1,1])
 #-----------------------------------------------------------------------------------------------------------
 OPTIM_SIZE=None
 def checkOptim(sc,scr,TOTAL_MIN):
     global OPTIM_SIZE
-    if (all(val >= THRESH for val in scr) and sum(list(sc))<TOTAL_MIN):
+    if (all(val > THRESH for val in scr) and sum(list(sc))<TOTAL_MIN):
         TOTAL_MIN =sum(list(sc))
-        OPTIM_SIZE=list(sc)
+        OPTIM_SIZE=np.asarray(sc)
     return TOTAL_MIN
-        
+
 def optimMan():
     __sizes=range(SC_SIZE_MIN,SC_SIZE_MAX+STEP_SIZE,STEP_SIZE)
     _list_sizes=[__sizes for _ in range(len(SC_IDS))]
     combs=list(itertools.product(*_list_sizes))
     size_vals=[]
     scr_vals=[]
-    col_names=['Size_{}'.format(SC_IDS[i]) for i in range(len(SC_IDS))]
-    col_names+=['SCR_{}'.format(SC_IDS[i]) for i in range(len(SC_IDS))]
-    LOG_INFO('Calculating SCR')
+    LOG_INFO('Calculating Initial Inclusive Sizes')
     TOTAL_MIN   =   SC_SIZE_MAX*len(SC_IDS)
     for sc in tqdm(combs):
-        for idx in range(len(SC_IDS)):
-            with silence():
-                psspy.machine_data_2(i=SC_IDS[idx],
-                                    id='1',
-                                    intgar=[1,0,0,0,0,0],
-                                    realar=[0,0,500,-200,0,0,sc[idx],0,0.09,0,0,1,1,1,1,1,1])
-
+        modifyCaseBySize(sc)
         scr=calcSCR(hv_bus_ids=BUS_IDS,gen_bus_ids=MACHINE_IDS)
         size_vals.append(list(sc))
         scr_vals.append(scr)
         TOTAL_MIN=checkOptim(sc,scr,TOTAL_MIN)
     
-    SIZES=np.vstack(size_vals)
-    SCRS=np.vstack(scr_vals)
-    DATA=np.concatenate((SIZES,SCRS),axis=1)
-    df = pd.DataFrame(data=DATA,columns=col_names)
-    _csv_path=os.path.join(os.getcwd(),'SCR.csv')
-    df.to_csv(_csv_path)
-    
-
-    
-
-
+def sizeSummary():
+    LOG_INFO('Sizes for Sync. Condensors',pcolor='yellow')
+    for idx in range(len(SC_IDS)):
+        print(colored('Sync. Cond. No.: {}        Size:  {}'.format(SC_IDS[idx],OPTIM_SIZE[idx]),color='yellow'))
+#-----------------------------------------------------------------------------------------------------------
 def calcSCR(hv_bus_ids,gen_bus_ids,ret_params=False):
     '''
     ARGS: 
@@ -212,16 +196,77 @@ def calcSCR(hv_bus_ids,gen_bus_ids,ret_params=False):
     else:
         return scr
     
-def printSummary():
-    LOG_INFO('Optimized Sizes for Sync. Condensors',pcolor='yellow')
-    for idx in range(len(SC_IDS)):
-        print(colored('Sync. Cond. No.: {}        Size:  {}'.format(SC_IDS[idx],OPTIM_SIZE[idx]),color='yellow'))
+#-----------------------------------------------------------------------------------------------------------
+# ---
+def fitness_function(X):
+    Y=[]
+    for x in X:
+        modifyCaseBySize(x)
+        scr=calcSCR(hv_bus_ids=BUS_IDS,gen_bus_ids=MACHINE_IDS)
+        if all(val > THRESH for val in scr):
+            y=np.sum(x)
+        else:
+            y=len(SC_IDS)*SC_SIZE_MAX
 
+        Y.append(y)
+    return np.asarray(Y)
+# ---
+def mutate(parents):
+    n=len(parents)
+    scores=fitness_function(parents)
+    #idx=scores < len(SC_IDS)*SC_SIZE_MAX
+    idx=scores > 0
+    scores=scores[idx]
+    parents=np.array(parents)[idx]
+    children=parents[np.random.choice(parents.shape[0],size=n,p=scores.astype('float32')/np.sum(scores))]
+    children=children+np.random.randint(low=-STEP_SIZE,high=STEP_SIZE,size=children.shape)
+    return children
+# ---
+def get_fitest_parents(parents):
+    _fitness=fitness_function(parents)
+    PFitness=list(zip(parents,_fitness))
+    PFitness.sort(key= lambda x:x[1]) # sort based on fitness
+    best_parent,best_fitness=PFitness[0]
+    return best_parent,best_fitness
+# ---    
+def GA(parents,max_iter):
+    LOG_INFO('Running Iterations')
+    global OPTIM_SIZE
+    best_parent,best_fitness=get_fitest_parents(parents)
+    PARENTS=[best_parent]
+    FITNESS=[best_fitness]
+    for i in tqdm(range(1,max_iter)):
+        parents=mutate(parents)
+        curr_parent,curr_fitness=get_fitest_parents(parents)
+        if curr_fitness < best_fitness:
+            best_fitness = curr_fitness
+            best_parent  = curr_parent
+        PARENTS.append(best_parent)
+        FITNESS.append(best_fitness)
+    OPTIM_SIZE=best_parent
+    return PARENTS,FITNESS
+    
+#-----------------------------------------------------------------------------------------------------------
+def main():
+    LOG_INFO('Initializing Population')
+    X=np.random.randint(low=SC_SIZE_MIN,high=SC_SIZE_MAX,size=(POP_SIZE-1,len(SC_IDS)))
+    X=np.vstack((X,INITIAL_SIZES))
+    sizes,totals=GA(X,MAX_ITER)
+    sizeSummary()
+    col_names=['Size_{}'.format(SC_IDS[i]) for i in range(len(SC_IDS))]
+    col_names+=['Total Size']
+    SIZES=np.vstack(sizes)
+    TOTALS=np.vstack(totals)
+    DATA=np.concatenate((SIZES,TOTALS),axis=1)   
+    df = pd.DataFrame(data=DATA,columns=col_names)
+    _csv_path=os.path.join(os.getcwd(),'history.csv')
+    df.to_csv(_csv_path)
+    df.plot()
+    plt.show()
 #-----------------------------------------------------------------------------------------------------------
 if __name__=='__main__':
     start_time=time()
     banner()
     initCase()
-    optimMan()
-    printSummary()
-    LOG_INFO('Time Taken:{}'.format(time()-start_time),pcolor='cyan')
+    main()
+    LOG_INFO('Time Taken:{} sec'.format(time()-start_time),pcolor='cyan')
